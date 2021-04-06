10e52500db50a84df8050eb1c79d0641948748a5
Realnaru <73569436+Realnaru@users.noreply.github.com>
2021-04-06 22:28:02 +0800

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,116 @@
+
+const englishStory = "Once upon time there lived a cat. His name was Murzik Petrovich. He was not an ordinary cat but a grandnephew of the godfather cat. He had a joyful, rich life by a compassionate old lady in one of Saint-Peterburg's yards. She adopted him when he was a kitten. She brought him up in the best traditions of Saint-Peterburg's intellectuals. That's why he never peed under a coach and never shrieked. Very rarely he sweared in cat language with stray cats from the Godfather cat's clan. And only not to be bullied as other usual house cats."
+
+const englishQuestions = "-Was there a cat?<br> -Yes, there was a cat.<br> -Was his name Murzik Petrovich?<br> -Yes, his name was Murzik Petrovich.<br> -Was he just an ordinary cat? No, he wasn't just an ordinary cat.<br> -How was his life? His life was free and rich.<br> -Where did he live? He lives with an old lady in one of Saint-Peterburg's yards.<br> -Was he brought up well? Yes, he was brought up in the best traditions of Saint-Peterburg's intellectuals</p>"
+
+const russianStory = "Жил-был кот, Мурзик Петрович. Кот он был не простой, внучатый племянник крестного котца. Жилось ему вольно, сытно и тепло у сердобольной старушки в одном из питерских дворов. Она подобрала его ещё котёнком, выкормила и воспитала в лучших традициях питерской интеллигенции. А потому Мурзик под диван не мочился, мебель не драл, дурным голосом не орал, а только ругался изредка благим кошачьим матом во дворе с бродячими котами из клана Крестного котца. Да и то, чтобы они его не задирали как обычных домашних котов."
+
+const russianQuestions = "-Жил-был кот?<br> -Да, жил-был кот.<br> -Его звали Мурзик Петрович?<br> -Да, его звали Мурзик Петрович.<br> -Это был простой кот?<br> -Нет, кот был не простой.<br> -Как ему жилось?<br> -Жилось ему вольно, сытно и тепло.<br> -Где он жил?<br> -Он жил у сердобольной старушки.<br> -Он был воспитан?<br> -Да, он был воспитан в лучших традициях питерской интеллигенции."
+
+const japaneseStory = "昔々、猫がそこに住んでいました。彼の名前はMurzikPetrovichでした。彼は普通の猫ではなく、ゴッドファーザー猫の祖父でした。 彼は思いやりのある老婦人によってサンクトペテルブルクの庭園の1つで幸せで繁栄した生活を送りました。彼女は彼が子猫だったときに彼を養子にした。 彼女はサンクトペテルブルクの知識人の最高の伝統で彼を育てました。それで彼はコーチの下でおしっこをしたり悲鳴を上げたりしませんでした。ごくまれに、 野良猫やゴッドファーザー一族の猫の言葉で誓った。そしてそれはあなたが他の通常の飼い猫のようにいじめられるのを防ぐ。";
+
+const japaneseQuestions = "-猫はいましたか？<br> -はい、猫がいました。<br> -彼の名前はMurzikPetrovichでしたか？<br> -はい、彼の名前はMurzikPetrovichでした。<br> -彼はただの普通の猫でしたか？<br> いいえ、彼はただの普通の猫ではありませんでした。<br> -彼の人生はどうでしたか？<br> 彼の人生は自由で豊かでした。<br> -彼がどこに住んでいました？<br> 彼はサンピエトロバグの庭の1つで老婦人と一緒に住んでいます。<br> -彼は元気に育ちましたか？<br> はい、彼はサンクトペテルブルクの知識人の最高の伝統で育ちました";
+
+const koreanStory = "옛날에 무르지크 페트로비츠라는 고양이가 있었다. 보통 고양이가 아니고 특별하고 대부라는 고양이의 삼종손이었다. 산트 페테르스부르그도시 계신 친절한 할머니 댁에 살았다. 할머니 댁에 고양이가 음식을 많이 먹고 따뜻한 장소에서 자고 여유있게 지냈다. 무르지크가 어린 나이에 할머니는 입양해서 피테르 인테리에 맞는 교육을 시켰다. 그래서 무르지크 소파 밑에 소변을 보지 않고 가구를 깔작대지 않고 큰 소리로 '야웅'하지 않았다. 그런데 가끔 마당에서 산책할 때 대부라는 고양이의 친한 집없는 고양이를 만나서 소리를 내면서 싸웠다. 다른 집있는 고양이들처럼 자생 고양이들 때문에 고민하지 않기 위해서이다.";
+
+const koreanQuestions = "-무르지크 씨는 어디에서 삽니까?<br> -어떻게 할머니를 만나게 됐습니까? <br>-산책할 때 누구랑 싸웁니까?<br> -고양이가 친절합니까?<br> -여러분 어떻게 생각합니까?";
+
+const chineseStory = "从前有一只公猫，它的名字是穆尔济可彼得洛维奇。它不是普通的猫，而是教父猫的外生。 它在圣彼得堡的一个院子里过着幸福的生活，它的主人是一位富有同情心的老太太。 那只公猫小的时候被老太太收养了。她以最良好的圣彼得堡传统养育了这只小猫。 因此它从来不在沙发下撒尿，从来不尖叫。有时它与教父猫氏族的流浪猫打架， 这样它就不会像其他普通家猫那样被欺负。";
+
+const chineseQuestions = "-从前，有着一只猫吗？<br> － 是的， -有着一只猫。<br> -它的名字是穆尔济可·彼得洛维奇（Mùěrjìkě ·Bǐdéluòwéiqí）吗？<br> -是的，它名字是穆尔济可·彼得洛维奇（Mùěrjìkě ·Bǐdéluòwéiqí<br> 它是普通一只猫吗?<br> 不是，它不是普通<br> 它是一只普通的猫吗？<br> 不，他不只是普通的猫。<br> 它的生活怎么样？<br> -它的生活又自由又富有。<br> 它住在哪里？<br> 它和老太太住在圣彼得堡的一个院子里。 它是有的猫吗？<br> -是的，她以最良好的圣彼得堡传统养育了这只小猫";
+
+document.querySelector("#rus").addEventListener("click", function(){
+  changeStoryLanguage(russianStory, russianQuestions);
+  changePageLanguage("rus");
+  changeAudioSourse("russian");
+});
+
+document.querySelector("#eng").addEventListener("click", function(){
+  changeStoryLanguage(englishStory, englishQuestions);
+  changePageLanguage("eng");
+  changeAudioSourse("english");
+});
+
+document.querySelector("#jpn").addEventListener("click", function(){
+  changeStoryLanguage(japaneseStory, japaneseQuestions);
+  changePageLanguage("jpn");
+  changeAudioSourse("japanese");
+});
+
+document.querySelector("#krn").addEventListener("click", function(){
+  changeStoryLanguage(koreanStory, koreanQuestions);
+  changePageLanguage("krn");
+  changeAudioSourse("korean");
+});
+
+document.querySelector("#chn").addEventListener("click", function(){
+  changeStoryLanguage(chineseStory, chineseQuestions);
+  changePageLanguage("chn");
+  changeAudioSourse("chinese");
+});
+
+document.querySelector("#listen-story-button").addEventListener("click", function(){
+  showHide("#storyAudio");
+});
+
+document.querySelector("#listen-questions-button").addEventListener("click", function(){
+  showHide("#questionsAudio");
+});
+
+
+
+function changeStoryLanguage(story, questions) {
+  document.querySelector(".main-text").innerText = story;
+  document.querySelector(".questions-text").innerHTML = questions;
+}
+
+function changePageLanguage(language){
+
+  switch (language) {
+    case "rus":
+      document.querySelector("#story-header").innerText = "История";
+      document.querySelector("#questions-header").innerText = "Вопросы и ответы";
+      document.querySelector("#listen-story-button").innerHTML = '<i class="fas fa-volume-up"></i> Слушать';
+      document.querySelector("#listen-questions-button").innerHTML = '<i class="fas fa-volume-up"></i> Слушать';;
+      break;
+
+    case "eng":
+      document.querySelector("#story-header").innerText = "Story";
+      document.querySelector("#questions-header").innerText = "Questions and answers";
+      document.querySelector("#listen-story-button").innerHTML = '<i class="fas fa-volume-up"></i> Listen';
+      document.querySelector("#listen-questions-button").innerHTML = '<i class="fas fa-volume-up"></i> Listen';
+      break;
+
+    case "jpn":
+      document.querySelector("#story-header").innerText = "物語";
+      document.querySelector("#questions-header").innerText = "質問と回答";
+      document.querySelector("#listen-story-button").innerHTML = '<i class="fas fa-volume-up"></i> 聞く';
+      document.querySelector("#listen-questions-button").innerHTML = '<i class="fas fa-volume-up"></i> 聞く';
+      break;
+
+    case "krn":
+      document.querySelector("#story-header").innerText = "이야기";
+      document.querySelector("#questions-header").innerText = "질문과 답변";
+      document.querySelector("#listen-story-button").innerHTML = '<i class="fas fa-volume-up"></i> 듣다';
+      document.querySelector("#listen-questions-button").innerHTML = '<i class="fas fa-volume-up"></i> 듣다';
+      break;
+
+    case "chn":
+      document.querySelector("#story-header").innerText = "故事";
+      document.querySelector("#questions-header").innerText = "问题和解答";
+      document.querySelector("#listen-story-button").innerHTML = '<i class="fas fa-volume-up"></i> 听';
+      document.querySelector("#listen-questions-button").innerHTML = '<i class="fas fa-volume-up"></i> 听';
+        break;
+    default:
+
+  }
+}
+
+function showHide(elementId){
+  document.querySelector(elementId).style.display = (document.querySelector(elementId).style.display === "block") ? "none": "block";
+}
+
+function changeAudioSourse(language) {
+  document.querySelector("#storyAudio").setAttribute("src", "sounds/" + language + "Story.mp3");
+  document.querySelector("#questionsAudio").setAttribute("src", "sounds/" + language + "Questions.mp3");
+}
